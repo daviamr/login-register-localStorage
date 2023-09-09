b747752bd38849b4ba712cdd776cf1da53f66d37
Davi (Estudo) <daviamorim_sz@live.com>
2023-09-09 03:29:55 -0300

    feat-remover-conta

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const acc_section = document.getElementById('acc_section');
 const data = JSON.parse(localStorage.getItem('acc_data')) || [];
 data.forEach(element => {
     if (data.length > 0) {
-        showAccount(element.usuario, element.senha)
+        showAccount(element.usuario, element.senha, element.id)
     }
 });
 
@@ -15,44 +15,68 @@ form.addEventListener('submit', (e) => {
 
     const userValue = user.value;
     const passValue = pass.value;
+    let exist = false;
 
 
     if (userValue === '' || passValue === '') {
         alert('Preencha corretamente.')
+        return
     };
-
-    let exist = true;
 
     for (i = 0; i < data.length; i++) {
         if (userValue === data[i].usuario) {
             alert('Usuário já cadastrado!');
-            exist = false;
-            break
-        } else {
-            var newUser = {
-                'usuario': userValue,
-                'senha': passValue
-            }
-            data.push(newUser);
-            localStorage.setItem('acc_data', JSON.stringify(data));
-            break
+            exist = true;
         }
     }
 
-    if (exist) {
-        showAccount(newUser.usuario, newUser.senha);
+    if (exist === false) {
+        createAccount();
+        removeAccount();
     }
 
     user.value = '';
     pass.value = '';
 })
-console.log(data)
 
-function showAccount(user, pass) {
-    const list = document.createElement('ul');
+function showAccount(user, pass, id) {
     const itemList = document.createElement('li');
-    list.appendChild(itemList)
-    itemList.innerText += `${user}, ${pass}`;
+    itemList.classList.add('li_item')
+    const itemListSpan = document.createElement('img');
+    itemListSpan.classList.add('remove');
+    itemListSpan.setAttribute('src', './img/close.svg')
+    itemList.innerHTML += `ID: ${id} | Usuário: ${user} / Senha: ${pass}`;
 
-    acc_section.appendChild(list);
+    itemList.appendChild(itemListSpan);
+    acc_section.appendChild(itemList);
 }
+
+function createAccount() {
+    const newUser = {
+        'id': data.length,
+        'usuario': user.value,
+        'senha': pass.value
+    }
+
+    data.push(newUser);
+    updateBank()
+    showAccount(newUser.usuario, newUser.senha, newUser.id);
+}
+
+
+function updateBank() {
+    localStorage.setItem('acc_data', JSON.stringify(data));
+}
+
+const item_list = document.getElementsByClassName('remove');
+function removeAccount() {
+    for (i = 0; i < item_list.length; i++) {
+        item_list[i].addEventListener('click', (e) => {
+            let target = e.target
+            target.parentNode.remove();
+            data.splice(target, 1)
+            updateBank();
+        })
+    }
+}
+removeAccount();
